6c64105c7d36354a1963967865d45b26976ca24c
rriveraro <rbrivera@estudiantes.unsam.edu.ar>
2024-10-29 15:41:43 -0300

    Trabajamos con la clase Celda

--- src/Celda.java
+++ src/Celda.java
@@ -1,5 +1,46 @@
 public class Celda<T> {
     private T valor;
 
-    
+    //Constructor 
+    public Celda(T valor){
+        this.valor = valor;
+    }
+
+    //Metodos
+    public T getValor(){
+        return valor;
+    }
+
+    public String getTipo() {
+        return valor.getClass().getSimpleName();
+    }
+
+    public boolean esNA(){
+        return valor==null;
+    }
+
+    public void cambiarValor(T valorNuevo){
+        this.valor=valorNuevo;
+    }
+
+
+
+    //PROBAMOS
+    public static void main(String[] args) {
+        Celda<Integer> celda = new Celda(0);
+
+        System.out.println("Valor: " + celda.getValor());
+        System.out.println("Tipo de dato: " + celda.getTipo());
+        System.out.println("Es NA?" + celda.esNA());
+        celda.cambiarValor(4);
+        System.out.println("Valor: " + celda.getValor());
+        
+        //String cadena;
+        //Celda<String> celda2 = new Celda<String>(cadena);
+        //System.out.println("V");
+
+
+    }
 }
+
+
--- src/Filtrable.java
+++ src/Filtrable.java
@@ -1,3 +1,4 @@
-public interface Filtrable {
+
+public interface Filtrable{
 
 }
