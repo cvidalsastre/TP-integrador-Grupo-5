ba10a7ab4ec948c6cebfd6c00d43f3afa343154a
adecheverri <alejandrodecheverri@gmail.com>
2024-10-29 17:58:41 -0300

    Algo cambió y no fui yo

--- src/Columna.java
+++ src/Columna.java
@@ -1,21 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class Columna<T> {
     
-    private List<Celda<T>> columna;
-    private T etiquetaColumna;
+    private List<Celda<T>> tirita;
+    private String etiquetaColumna;
 
     //Constructor
-    public Columna() {
+    public Columna(s) {
         this.columna = new ArrayList<>();
+        this.etiquetaColumna = new String("");
     }
 
     public Columna(List<T> columna){
         this.columna = new ArrayList<>();
         for(T elemento : columna){
             columna.add(elemento);
-        }        
+        }
+        this.etiquetaColumna = new String();        
+    }
+
+    public void cambiarNombreEtiqueta(String etiqueta){
+        this.etiquetaColumna = etiqueta;
+    }
+
+    public T verContenidoCelda(int pos){
+        return columna.get(pos).getValor();
+    }
+
+    public String tipoDato
+
+    public int cantidadElementos(){
+        return this.columna.size();
     }
 
     
