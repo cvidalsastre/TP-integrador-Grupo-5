be219fbbeedc901997555b4795a12a91ae0c975a
rriveraro <rbrivera@estudiantes.unsam.edu.ar>
2024-10-29 16:14:36 -0300

    Trabajamos con la clase columna

--- src/Columna.java
+++ src/Columna.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class Columna {
+public class Columna<T> {
     
-    private List<Celda> columna;
-    private List<String> etiquetaColumna;
+    private List<Celda<T>> columna;
+    private T etiquetaColumna;
+
+    //Constructor
+    public Columna() {
+        this.columna = new ArrayList<>();
+    }
+
+    public Columna(List<T> columna){
+        this.columna = new ArrayList<>();
+        for(T elemento : columna){
+            columna.add(elemento);
+        }        
+    }
+
+    public Columna(List<T> columna, String label){
+        this.columna = new ArrayList<>();
+        for(T elemento : columna){
+            columna.add(elemento);
+        }        
+    }
+
+
 
 }
