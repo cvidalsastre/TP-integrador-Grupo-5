002243e4eac23719fef72f279c82fc773a50f736
cvidalsastre <cvidalsastre@unsam.edu.ar>
2024-11-10 21:27:28 -0300

    agrego dataset 100k y limpio algunas referencias q ya no se usan

--- src/Agrupable.java
+++ src/Agrupable.java
@@ -2,7 +2,5 @@ import java.util.List;
 import java.util.Map;
 
 public interface Agrupable {
-    Map<List<Object>, List<Integer>> agruparPor(List<Etiqueta> etiquetasColumnas);
 
-    Tabla aplicarOperaciones(Map<List<Object>, List<Integer>> grupos, String operacion);
 }
